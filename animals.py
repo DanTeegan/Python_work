8297c6ce9a168ef74ef6da2026ba60ef3c9f88bd
dan. teegan <dan.teegan@hotmail.com>
2020-07-02 17:59:16 +0100

    Python

---
+++ animals.py
@@ -0,0 +1,18 @@
+# Here we are creating the class Animals
+class Animal:
+
+    # Here we are are creating a variable
+    # animal_type = "animal"
+
+    def __init__(self, name, color, mood, hunger, sleep, breath):
+        self.name = name
+        self.color = color
+        self.mood = mood
+        self.hunger = hunger
+        self.sleep = sleep
+        self.breath = breath
+
+    def breath
+
+
+
