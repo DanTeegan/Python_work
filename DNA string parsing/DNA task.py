ec256764c4dc997e8c70192ae55ac9c905790bb8
dan. teegan <dan.teegan@hotmail.com>
2020-07-04 19:01:54 +0100

    Fizz Buzz finder

--- DNA string parsing/DNA task.py
+++ DNA string parsing/DNA task.py
@@ -1,1 +1,30 @@
 
+class Dna:
+    def dna_counter(self):
+
+        dna = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+        A = []
+        C = []
+        G = []
+        T = []
+
+        for letter in dna:
+            if letter == "A":
+                A.append(letter)
+            if letter == "C":
+                C.append(letter)
+            if letter == "G":
+                G.append(letter)
+            if letter == "T":
+                T.append(letter)
+
+        A_count = len(A)
+        C_count = len(C)
+        G_count = len(G)
+        T_count = len(T)
+
+        print(A_count, C_count, G_count, T_count)
+
+dna1 = Dna()
+dna1.dna_counter()
---
+++ Fizz_Buzz/fizz_buzz.py
@@ -0,0 +1,25 @@
+
+
+# If divisible by 3 % 3 = 0
+# If divisible by 5 % 5 = 0
+
+
+
+
+class fizz_buzz:
+
+    def fizz_buzz_finder(self):
+
+        for number in range(1, 101):
+            if number % 3 == 0:
+                print("Fizz")
+            elif number % 5 == 0:
+                print("Buzz")
+            elif number % 3 == 0 and number % 5 == 0:
+                print("Fizz-Buzz")
+            else:
+                print(number)
+
+
+fizzbuzz1 = fizz_buzz()
+fizzbuzz1.fizz_buzz_finder()
